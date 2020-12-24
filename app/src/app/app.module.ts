6a703b5a7104bfb519e20b4ea4607a3b7beb0dc3
Raul Sibaja <raulsibaja7@gmail.com>
2020-12-23 19:57:38 -0600

    intento api

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -9,13 +9,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [HttpClientModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
+    HttpClient,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- app/src/app/tab1/tab1.page.ts
+++ app/src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +10,16 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  criptomonedas: any = [];
+
+  runHttp(){
+    this.http.get('https://api.bitso.com/v3/available_books/')
+      .subscribe(data=> {
+        console.log(data);
+        this.criptomonedas = data;
+      })
+  }
 
 }
